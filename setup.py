062364412e01540d30d64ad47a7deb263129b3cc
Swaroop kumar <68234285+swaroopyadav49@users.noreply.github.com>
2024-11-12 17:38:00 +0530

    Updated

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject_regression',
     version= '0.0.1',
-    author='Utkarsh Gaikwad',
-    author_email='gaikwadujg@gmail.com',
+    author='kumar',
+    author_email='gskyadav91@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
